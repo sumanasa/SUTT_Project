c0a06e82aeb853b7c2f2e7a042f7865eba8213d9
somu sumanasa <sumanasasomu@yahoo.com>
2018-07-18 12:11:49 +0530

    changes1

--- app/src/main/java/com/example/win10/su/Main2Activity.java
+++ app/src/main/java/com/example/win10/su/Main2Activity.java
@@ -56,13 +56,13 @@ public class Main2Activity extends AppCompatActivity implements NavigationView.O
         }
         return super.onOptionsItemSelected(item);
     }
-    @Override
-    public void onBackPressed() {
-        if (DrLt.isDrawerOpen(GravityCompat.START)) {
-            DrLt.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+//        if (DrLt.isDrawerOpen(GravityCompat.START)) {
+//            DrLt.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 }
 
--- app/src/main/java/com/example/win10/su/MainActivity.java
+++ app/src/main/java/com/example/win10/su/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
 
         userid = (EditText) findViewById(R.id.username);
